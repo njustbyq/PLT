82a09803761a3ae808bc1ca29314f67814952714
njustbyq <2042675095@qq.com>
2021-11-08 23:24:38 +0800

    ex13/14

---
+++ HTDP/Chapter2/ex13.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex13) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Define the function string-first, which extracts the first
+; lString from a non-empty string.
+(define (string-first str)
+  (substring str 0 1))
+
+(string-first "hello")
+(string-first "lemon")
