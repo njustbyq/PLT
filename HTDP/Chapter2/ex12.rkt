d01c9eb579526a65cbbe7beae48f09156732f43d
njustbyq <2042675095@qq.com>
2021-11-08 23:06:54 +0800

    exercise for chapter2

---
+++ HTDP/Chapter2/ex12.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex12) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Define the function cvolume, which accepts the length of a side
+; of an equilateral cube and computes its volume.
+(define (cvolume side)
+  (* side side side))
+
+(cvolume 1)
+(cvolume 5)
+(cvolume 10)
