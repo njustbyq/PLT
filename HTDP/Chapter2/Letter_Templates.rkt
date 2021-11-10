0934aa848f4575e638ade6e584213b203d212a25
njustbyq <2042675095@qq.com>
2021-11-10 23:11:51 +0800

    Composting_Functions

---
+++ HTDP/Chapter2/Letter_Templates.rkt
@@ -0,0 +1,32 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Letter_Templates) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Programs usually consist of a main function that calls helper functions
+; Define one function per task.
+ Define one function per task.
+(require 2htdp/batch-io)
+(define (letter fst lst signature-name)
+  (string-append
+     (opening fst)
+     "\n\n"
+     (body fst lst)
+     "\n\n"
+     (closing signature-name)))
+
+(define (opening fst)
+  (string-append "Dear " fst ","))
+
+(define (body fst lst)
+  (string-append
+   "We have discovered that all people with the" "\n"
+   "last name " lst " have won our lottery. So, " "\n"
+   fst ", " "hurry and pick up your prize."))
+
+(define (closing signature-name)
+  (string-append
+   "Sincerely,"
+   "\n\n"
+   signature-name
+   "\n"))
+
+(write-file 'stdout(letter "Matthew" "Fisler" "Felleisen"))
